d5af4db4f0d9c4dcc1bc82a18540bdae0f29fc9f
Lefuden <84024382+Lefuden@users.noreply.github.com>
2023-09-12 16:05:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿//Daniel Frykman, .NET23
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //set a number.
+            int myNumber = 7;
+            
+            //if-else to check how big the number is and output a message depending on result.
+            if(myNumber > 10)
+            {
+                Console.WriteLine("Talet är stort!");
+            }
+            else
+            {
+                Console.WriteLine("Oj. Lågt tal!");
+            }
+
+            //initiate variable for input. Ask user's name, add input to yourName. Output message containing the user's name.
+            string yourName;
+
+            Console.WriteLine("\nVad heter du?");
+            Console.Write(">");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine($"Hej {yourName}!\n");
+
+            //loop through numbers from zero to number set in myNumber.
+            for (int i = 0; i <= myNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
